ed6cdb1d057fb26b2d67b45e8f1e73ba01ff3ccf
Pachwenko <32424503+Pachwenko@users.noreply.github.com>
2019-03-12 22:35:26 -0500

    began tutorial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # tkinter-text-editor
-A text editor in Python 3.6+ with Tkinter, tutorial by pymike00 on youtube
+A text editor in Python 3.6+ built with Tkinter by folowing the tutorial by pymike00 on youtube.
+
+## Dependencies
+
+Python3.6 or higher (for ubuntu, sudo apt install python3)
+Tkinter (for ubuntu, sudo apt install python3-tk)
+
+## How to run
+
+```bash
+python3 editor.py
+```
---
+++ editor.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+
+"""
+
+Text editor in python using Tkinter. Follows pymike00 on youtube
+https://www.youtube.com/watch?v=7PGFin30c4o&t=230s
+
+"""
+
+class Editor:
+    """
+    Main class for the text editor.
+    """
+
+    def __init__(self, master):
+        """
+        Initializes the text editor.
+        Args:
+            master (tkinter instanse): the main window of the application.
+        """
+        # set the title and initial size
+        master.title("Untitled - Editor")
+        master.geometry("1200x700")
+
+        # initalize the text widget
+        self.textarea = tk.Text(master)
+        # allow scroling with scrollbar widget
+        self.scroll = tk.Scrollbar(master, command=self.textarea.yview)
+        # set up scrolling with mouse
+        self.textarea.configure(yscrollcommand=self.scroll.set)
+
+
+
+if __name__ == "__main__":
+    """
+    Runs main loop of the program
+    """
+    master = tk.Tk()
+    pt = Editor(master)
+    master.mainloop()
